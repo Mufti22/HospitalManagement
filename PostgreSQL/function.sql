26ae642f34c6368b6a961b8ec120236e4ee9f50f
Malik Fomin <mr.baharov@gmail.com>
2022-01-11 01:40:28 +0700

    Day 6, adding Time

--- PostgreSQL/function.sql
+++ PostgreSQL/function.sql
@@ -1,2 +1,7 @@
 alter table user1
     add constraint type1 check ( type = 'headdoctor' or type = 'doctor' or type = 'patient' );
+
+alter table whour
+    add constraint status check ( status = 'a' or status = 'p' );
+
+ALTER TABLE whour ALTER COLUMN status SET DEFAULT 'a';
